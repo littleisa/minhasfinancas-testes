b145aae1631108d4f361e4c6dbbe9a68869f9683
littleisa <isamilu3101@gmail.com>
2026-04-30 00:48:32 -0400

    testes unitários das regras de negócio

---
+++ backend-tests/MinhasFinancas.Tests/PessoaTests.cs
@@ -0,0 +1,74 @@
+﻿using MinhasFinancas.Domain.Entities;
+
+namespace MinhasFinancas.Tests;
+
+public class PessoaTests
+{
+    [Fact]
+    public void Pessoa_MenorDeIdade_NaoEhMaiorDeIdade()
+    {
+        // Arrange
+        var pessoa = new Pessoa
+        {
+            Nome = "Yasmin",
+            DataNascimento = DateTime.Today.AddYears(-17)
+        };
+
+        // Act
+        var resultado = pessoa.EhMaiorDeIdade();
+
+        // Assert
+        Assert.False(resultado);
+    }
+
+    [Fact]
+    public void Categoria_DeReceita_NaoPermiteDespesa()
+    {
+        // Arrange
+        var categoria = new Categoria
+        {
+            Descricao = "Salário",
+            Finalidade = Categoria.EFinalidade.Receita
+        };
+
+        // Act
+        var resultado = categoria.PermiteTipo(Transacao.ETipo.Despesa);
+
+        // Assert
+        Assert.False(resultado);
+    }
+
+    [Fact]
+    public void Categoria_DeAmbas_PermiteReceita()
+    {
+        // Arrange
+        var categoria = new Categoria
+        {
+            Descricao = "Outros",
+            Finalidade = Categoria.EFinalidade.Ambas
+        };
+
+        // Act
+        var resultado = categoria.PermiteTipo(Transacao.ETipo.Receita);
+
+        // Assert
+        Assert.True(resultado);
+    }
+
+    [Fact]
+    public void Pessoa_MaiorDeIdade_EhMaiorDeIdade()
+    {
+        // Arrange
+        var pessoa = new Pessoa
+        {
+            Nome = "Yasmin",
+            DataNascimento = DateTime.Today.AddYears(-20)
+        };
+
+        // Act
+        var resultado = pessoa.EhMaiorDeIdade();
+
+        // Assert
+        Assert.True(resultado);
+    }
+}
